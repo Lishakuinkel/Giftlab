fcc09273e68ad8aac03834e41779fdebdaf5f7bc
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-23 14:52:07 -0600

    updated user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+//imported bcrypt libray and Oder from Order model
+const bcrypt = require('bcrypt');
+const Order = require('./Order');
+
 const userSchema = new Schema({
   username: {
     type: String,
@@ -20,6 +24,22 @@ const userSchema = new Schema({
   orders: [Order.schema]
 });
 
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare entered password with bcrypted password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+
+//export User model
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
