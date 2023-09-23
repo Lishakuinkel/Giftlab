ac1635b76d10a39a4771198551950a312326de3c
Lisha Kuinkel <ltkuinkel@gmail.com>
2023-09-23 17:00:45 -0600

    started working on react front end - created navbar with linked logo, login and cart pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Cart from './pages/cart/Cart';
+import Login from './pages/login/login';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path ='/login' element={<Login />} />
+          <Route path = '/cart' element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import { ShoppingCart, User } from 'phosphor-react';
+import "./Navbar.css";
+import logo from '../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <a href="/"><img className='logo' src={logo} alt='logo' /></a>
+      <div className='links'>
+        
+        <Link to ='/login'>
+          <User size={32} />
+        </Link>
+        <Link to ='/cart'>
+          
+          <ShoppingCart size = {32} />
+        </Link>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
