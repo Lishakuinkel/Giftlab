af12a950c0a06565696658a3c8719670c26aa195
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-26 11:31:32 -0600

    connected apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client';
 import Navbar from './components/Navbar';
 import Cart from './pages/cart/Cart';
 import Login from './pages/login/login';
 import Signup from './pages/signup/signup';
-import Home from './components/Home';
-// import Logout from './pages/Logout/Logout';
+import { setContext } from '@apollo/client/link/context';
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div>
       
       <div>
@@ -28,6 +49,7 @@ function App() {
       </div>
 
     </div>
+    </ApolloProvider>
   );
 }
 
