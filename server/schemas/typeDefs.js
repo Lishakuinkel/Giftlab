d3a38779ee1daf307ba35e1e7e006a4d1000448d
suzyhan <129558186+suzyhan@users.noreply.github.com>
2023-09-25 18:39:04 -0600

    Add to typeDefs, update resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,6 +14,54 @@ type User {
     user: User
   }
 
-  `
+type Category {
+  _id: ID
+  name: String
+}
+
+type Product {
+  _id: ID
+  name: String
+  description: String
+  image: String
+  quantity: Int
+  price: Float
+  category: Category
+}
+
+input productInput {
+  _id: ID
+  purchaseQuantity: Int
+  name: String
+  image: String
+  price: Float
+  quantity: Int
+}
+
+type Order {
+  _id: ID
+  purchaseDate: String
+  products: [Product]
+  total_price: Int
+  user: String
+}
+
+type Query {
+  categories: [Category]
+  products(category: ID, name: String): [Product]
+  product(_id: ID!): Product
+  user: User
+  order(_id: ID!): Order
+  checkout(products: [ProductInput]): Checkout
+}
+
+type Mutation {
+  addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+  addOrder(products: [ID]!): Order
+  updateUser(firstName: String, lastName: String, email: String, password: String): User
+  updateProduct(_id: ID!, quantity: Int!): Product
+  login(email: String!, password: String!): Auth
+}
+`;
 
   module.exports = typeDefs;
