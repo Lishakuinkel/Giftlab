97e5e6612876f970783978e0c8f614560a5a6dd2
suzyhan <129558186+suzyhan@users.noreply.github.com>
2023-09-27 21:13:19 -0600

    Update and rename queries for products

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,26 +1,9 @@
 import { gql } from '@apollo/client';
 
-// query to get a list of all products
-export const GET_PRODUCTS = gql`
-  query getProducts {
-    products {
-      _id
-      name
-      description
-      price
-      quantity
-      image
-      category {
-        name
-      }
-    }
-  }
-`;
-
-// query to get a product by id
-export const GET_PRODUCT_BY_ID = gql`
-  query getProductById($_id: ID) {
-    product(_id: $_id) {
+// query a product by id
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
       _id
       name
       description
@@ -29,6 +12,60 @@ export const GET_PRODUCT_BY_ID = gql`
       image
       category {
         _id
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ProductInput]) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+// query all products
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      quantity
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+      image
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      username
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
       }
     }
   }
