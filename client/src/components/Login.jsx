d433f0c632b1ebc4e2ce2566f7b41f2207bb7245
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-30 12:52:31 -0600

    deleted react logo, added mutation to login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import { loginFields } from "../constants/formFields";
+import {useMutation} from "@apollo/client";
+import {LOGIN} from "../utils/mutations";
 import Input from "./Input";
 import FormAction from "./FormAction";
 import FormExtra from "./FormExtra";
+import Auth from "../utils/auth"
 
 import { useNavigate } from "react-router-dom";
 
@@ -11,23 +14,38 @@ let fieldsState = {};
 fields.forEach(field => fieldsState[field.id] = '');
 
 export default function Login() {
-    const [loginState, setLoginState] = useState(fieldsState);
+    const [loginState, setLoginState] = useState({username: '', password: ''});
     const navigate = useNavigate();
 
     const handleChange = (e) => {
         setLoginState({ ...loginState, [e.target.id]: e.target.value })
     }
+    // add LOGIN Mutation
+    const [login,{error,data}]= useMutation(LOGIN);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //authenticateUser();
-        
+    const handleSubmit = async event => {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables: { username: loginState.username, password: loginState.password}
+            })
+            const token = mutationResponse.data.login.token;
+
+            Auth.login (data.login.token);
+
+            alert('Login successful');
+
+            navigate('/signup');
+        } catch (error) {
+            console.log(error);
+        }
+        //authenticateUser();     
     }
 
     //Handle Login API Integration here
-    const authenticateUser = () => {
+    // const authenticateUser = () => {
+    // }
 
-    }
 
 
     return (
