5d07f81e8256dd3abeec1ef9589edb9b32c3c9ea
Lisha Kuinkel <ltkuinkel@gmail.com>
2023-09-30 21:30:10 -0600

    created home page and started working on it

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Products from '../../components/ProductList/Products'
+//import Products from '../../components/ProductList/Products'
 //import CategoryMenu from '../../components/CategoryMenu/CategoryMenu';
 
 const Home = () => {
   return (
     
     <div> 
-      <p>Homepage....</p>
       
-      <Products />
+      <h1 className=''>Where Thoughtful Gifts Come to Life</h1>
+       
     </div>
   )
 }
 
-export default Home
+export default Home    ij1
