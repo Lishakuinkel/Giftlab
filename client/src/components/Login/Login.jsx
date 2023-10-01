4f49980412dd354696b4081f35379b2a9617f40e
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-30 18:39:07 -0600

    fixed the login issues

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useMutation } from '@apollo/client';
-import { LOGIN } from '../../utils/mutations';
-import Auth from '../../utils/auth';
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../../utils/mutations";
+import Auth from "../../utils/auth";
 
 import { loginFields } from "../../constants/formFields";
 import Input from "../Form/Input";
@@ -13,69 +13,69 @@ const fields = loginFields;
 let fieldsState = {};
 
 //clear form values
-fields.forEach(field => fieldsState[field.id] = '');
+fields.forEach((field) => (fieldsState[field.id] = ""));
 
 export default function Login() {
-    const [loginState, setLoginState] = useState(fieldsState);
-    //const [validated] = useState(false);
-    const [showAlert, setShowAlert] = useState(false);
-    const navigate = useNavigate();
+  const [loginState, setLoginState] = useState(fieldsState);
+  //const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const navigate = useNavigate();
 
-    const [login, { error, data }] = useMutation(LOGIN);
+  const [login, { error, data }] = useMutation(LOGIN);
 
-    // update state based on form input changes
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setLoginState({
-            ...loginState,
-            [name]: value,
-        });
-    };
+  useEffect(() => {
+    console.log("Auth.loggedIn(): ", Auth.loggedIn());
+    if (Auth.loggedIn()) {
+      navigate("/profile");
+    }
+  }, []);
 
-    // submit form
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(loginState);
+  // update state based on form input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginState({
+      ...loginState,
+      [name]: value,
+    });
+  };
 
-        try {
-            const { data } = await login({
-                variables: { ...loginState },
-              });
-        
-              Auth.login(data.login.token);
+  // submit form
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(loginState);
 
-              
-            } catch (e) {
-              console.error(e);
-              navigate('/');
-            }
+    try {
+      const { data } = await login({
+        variables: { ...loginState },
+      });
 
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+      navigate("/");
+    }
+  };
 
-    return (
-        <form className="mt-8 space-y-6 w-96 mx-auto" onSubmit={handleSubmit}>
-            <div className="-space-y-px">
-                {
-                    fields.map(field =>
-                        <Input
-                            key={field.id}
-                            handleChange={handleChange}
-                            value={loginState[field.id]}
-                            labelText={field.labelText}
-                            labelFor={field.labelFor}
-                            id={field.id}
-                            name={field.name}
-                            type={field.type}
-                            isRequired={field.isRequired}
-                            placeholder={field.placeholder}
-                        />
-
-                    )
-                }
-            </div>
-            <FormExtra />
-            <FormAction handleSubmit={handleSubmit} text="Login" />
-
-
-        </form>
-    )
-}}
+  return (
+    <form className="mt-8 space-y-6 w-96 mx-auto" onSubmit={handleSubmit}>
+      <div className="-space-y-px">
+        {fields.map((field) => (
+          <Input
+            key={field.id}
+            handleChange={handleChange}
+            value={loginState[field.id]}
+            labelText={field.labelText}
+            labelFor={field.labelFor}
+            id={field.id}
+            name={field.name}
+            type={field.type}
+            isRequired={field.isRequired}
+            placeholder={field.placeholder}
+          />
+        ))}
+      </div>
+      <FormExtra />
+      <FormAction handleSubmit={handleSubmit} text="Login" />
+    </form>
+  );
+}
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,27 +1,26 @@
 // eslint-disable-next-line no-unused-vars
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-import { ShoppingCart, User } from 'phosphor-react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart, User } from "phosphor-react";
 import "./Navbar.css";
-import logo from '../../assets/logo.png'
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <a href="/"><img className='logo' src={logo} alt='logo' /></a>
-      <div className='links'>
-        
-        <Link to ='/login'>
+    <div className="navbar">
+      <a href="/">
+        <img className="logo" src={logo} alt="logo" />
+      </a>
+      <div className="links">
+        <Link to="/login">
           <User size={32} />
         </Link>
-        <Link to ='/cart'>
-          
-          <ShoppingCart size = {32} />
+        <Link to="/cart">
+          <ShoppingCart size={32} />
         </Link>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
