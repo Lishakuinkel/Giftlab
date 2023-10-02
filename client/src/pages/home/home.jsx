40b1ff8862dca12482c0c7ecd28d97d9d8ece29e
Lisha Kuinkel <ltkuinkel@gmail.com>
2023-10-01 22:59:42 -0600

    added multi-carousel products component on homepage

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import Typed from "react-typed";
-// import { Gift } from 'phosphor-react';
 import Searchbar from "./searchbar";
 
 import Products from "../../components/ProductList/Products";
-import CategoryMenu from "../../components/CategoryMenu/CategoryMenu";
+
 
 const Home = () => {
   const [searchFilter, setSearchFilter] = useState(null);
@@ -12,17 +11,17 @@ const Home = () => {
   return (
     <div className="bg-[#eaeadb] w-full h-screen text-center">
       <Searchbar setSearchFilter={setSearchFilter} />
-      <div className="py-2 items-center text-center">
+      <div className="py-2 items-center text-center text-2xl md:text-2xl sm:text-1xl">
         <Typed
-          className="font-bold font-style: italic font-serif text-[#c77783] drop-shadow-lg "
-          strings={["Where thoughtful gifts come to life..."]}
+          className=" font-bold font-style: italic font-serif text-[#c77783] drop-shadow-lg "
+          strings={["where thoughtful gifts come to life..."]}
           typeSpeed={80}
           backSpeed={100}
           loop
         />
       </div>
       <div>
-        <CategoryMenu />
+        
         <Products filter={searchFilter} />
       </div>
     </div>
