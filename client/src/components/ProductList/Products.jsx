f6623a58d198a9f5d2acbf6f700c701aee0fdd68
Aleenabrink93 <aleenabrink@gmail.com>
2023-10-01 19:37:25 -0600

    added filter function to search bar

--- client/src/components/ProductList/Products.jsx
+++ client/src/components/ProductList/Products.jsx
@@ -1,26 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 //import ProductItem from '../ProductItem/ProductItem';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import { useMemo } from "react";
 
-function ProductList() {
+function ProductList({ filter }) {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  const ProductList = data?.products || [];
 
-  console.log(ProductList);
+  // const products = data?.products || [];
+
+  const products = useMemo(() => {
+    if (!data) {
+      return [];
+    }
+    if (!data.products) {
+      return [];
+    }
+    if (filter && filter.name) {
+      return data.products.filter((product) =>
+        product.name
+          .toLocaleLowerCase()
+          .includes(filter.name.toLocaleLowerCase())
+      );
+    }
+
+    return data.products;
+  }, [data, filter]);
+
+  console.log(products);
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {
-        ProductList?.map((product, i) => {
-          return (
-            <button key={i} className=''>{product.name}</button>
-          )
-        })
-      }
+      {products?.map((product, i) => {
+        return (
+          <button key={i} className="">
+            {product.name}
+          </button>
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default ProductList;
--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import Typed from 'react-typed';
-import { Gift } from 'phosphor-react';
-import Searchbar from './searchbar';
+import React, { useState } from "react";
+import Typed from "react-typed";
+// import { Gift } from 'phosphor-react';
+import Searchbar from "./searchbar";
 
-
-import Products from '../../components/ProductList/Products'
-import CategoryMenu from '../../components/CategoryMenu/CategoryMenu';
+import Products from "../../components/ProductList/Products";
+import CategoryMenu from "../../components/CategoryMenu/CategoryMenu";
 
 const Home = () => {
+  const [searchFilter, setSearchFilter] = useState(null);
+
   return (
-    
-    <div className='bg-[#eaeadb] w-full h-screen text-center'> 
-    <Searchbar />
-      <div className='py-2 items-center text-center'>
-     
-      <Typed className='font-bold font-style: italic font-serif text-[#c77783] drop-shadow-lg ' strings={['Where thoughtful gifts come to life...']} typeSpeed={80} backSpeed={100} loop />
+    <div className="bg-[#eaeadb] w-full h-screen text-center">
+      <Searchbar setSearchFilter={setSearchFilter} />
+      <div className="py-2 items-center text-center">
+        <Typed
+          className="font-bold font-style: italic font-serif text-[#c77783] drop-shadow-lg "
+          strings={["Where thoughtful gifts come to life..."]}
+          typeSpeed={80}
+          backSpeed={100}
+          loop
+        />
       </div>
       <div>
-       <CategoryMenu /> 
-       <Products />
+        <CategoryMenu />
+        <Products filter={searchFilter} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home 
+export default Home;
