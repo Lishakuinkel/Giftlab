1a4ab76c07ecbeb134dc37a6dbd2dc9b5e800991
Anna Gallagher <annagallagher713@gmail.com>
2023-10-02 10:09:48 -0600

    added order history

---
+++ client/src/components/OrderHistory/OrderHistory.jsx
@@ -0,0 +1,12 @@
+function OrderHistory () {
+
+    // TODO: Use the useQuery hook from apollo to get order history data from the back-end
+
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+};
+
+export default OrderHistory;
