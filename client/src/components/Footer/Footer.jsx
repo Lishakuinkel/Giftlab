f2fcd03d73e300d18d7740b58607c385d84910ff
Aleenabrink93 <aleenabrink@gmail.com>
2023-10-04 09:33:42 -0600

    fixed footer position

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,21 +1,34 @@
-import { GithubLogo, FacebookLogo, InstagramLogo } from 'phosphor-react';
-import React from 'react'
+import { GithubLogo, FacebookLogo, InstagramLogo } from "phosphor-react";
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div className='text-center'> 
-            <div className='bg-[#577c72] w-full mx-auto py-1 px-2 grid lg:grid-cols-3 text-gray-300 '>
-            <p> ©2023 GIFTLAB created by Anna, Suzy, Lisha  & Aleena</p>
-            <div>
-                <ul className='flex justify-between mx-auto w-[75%] py-4'>
-                    <li><a href="https://www.facebook.com/"><FacebookLogo size={30} /></a></li>
-                    <li><a href="https://www.instagram.com/"><InstagramLogo size={30} /></a></li>
-                    <li><a href="https://github.com/Lishakuinkel/giftlab"><GithubLogo size={30} /></a></li>
-                </ul>
+  return (
+    <div className="text-center footer">
+      <div className="bg-[#577c72] w-full mx-auto py-1 px-2 grid lg:grid-cols-3 text-gray-300 ">
+        <p> ©2023 GIFTLAB created by Anna, Suzy, Lisha & Aleena</p>
+        <div>
+          <ul className="flex justify-between mx-auto w-[75%] py-4">
+            <li>
+              <a href="https://www.facebook.com/">
+                <FacebookLogo size={30} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/">
+                <InstagramLogo size={30} />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/Lishakuinkel/giftlab">
+                <GithubLogo size={30} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-            </div>
-        </div></div>
-    )
-}
-
-export default Footer
+export default Footer;
