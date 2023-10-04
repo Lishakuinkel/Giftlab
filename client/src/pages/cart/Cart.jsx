d0dedbb0412910a939d691a16e1d16b1f1e5d5dc
Aleenabrink93 <aleenabrink@gmail.com>
2023-10-04 12:08:21 -0600

    prompt login before checkout; save cart to LS

--- client/src/pages/cart/Cart.jsx
+++ client/src/pages/cart/Cart.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import { useCartContext } from '../../utils/cartContext';
-import { useMutation } from '@apollo/client';
+import React from "react";
+import { useCartContext } from "../../utils/cartContext";
+import { useMutation } from "@apollo/client";
 import "./cart.css";
 // import Checkout from '../checkout/Checkout'
 
-import { ADD_ORDER } from '../../utils/mutations';
-import { useNavigate } from 'react-router-dom';
-
+import { ADD_ORDER } from "../../utils/mutations";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cart, totalAmount, addToCart, removeCart } = useCartContext();
-  
+
   const [addOrder] = useMutation(ADD_ORDER);
   const navigate = useNavigate();
 
@@ -18,42 +17,48 @@ const Cart = () => {
 
   const checkout = async () => {
     try {
-      const productsIds = cart.map((item) => item.product._id)
+      const productsIds = cart.map((item) => item.product._id);
+      localStorage.setItem("product", productsIds);
+      localStorage.setItem("totalAmount:", totalAmount);
       console.log("productsIds:", productsIds);
       console.log("totalAmount:", totalAmount);
       const { data } = await addOrder({
-        variables: { products: productsIds, total_price: totalAmount }
-
-      })
-
-      navigate("/checkout")
-      console.log('graphql response:', data);
+        variables: { products: productsIds, total_price: totalAmount },
+      });
+      navigate("/checkout");
+      console.log("graphql response:", data);
     } catch (error) {
-      console.log('graphql error:', error);
+      alert("Please sign in before checkout");
+      navigate("/login");
+      console.log("graphql error:", error);
     }
-  }
+  };
 
   return (
     <div>
-      <h2 className='text-center py-8'>Your shopping cart</h2>
+      <h2 className="text-center py-8">Your shopping cart</h2>
       <div>
         <ul>
           {cart.map((item) => (
             <li className="card" key={item.product._id}>
               {item.product.name} - Quantity: {item.quantity}
               <div>
-
                 <p className="price">${item.product.price}</p>
               </div>
-              <button onClick={() => removeCart(item.product._id)}>Remove</button>
+              <button onClick={() => removeCart(item.product._id)}>
+                Remove
+              </button>
               <button onClick={() => addToCart(item.product)}>Add</button>
             </li>
           ))}
         </ul>
       </div>
-      <div className='text-center py-10'>
-        <p>Total Amount: ${totalAmount.toFixed(2)}</p> {/* Display the total amount */}
-        <button className="checkout" onClick={checkout}>Checkout</button>
+      <div className="text-center py-10">
+        <p>Total Amount: ${totalAmount.toFixed(2)}</p>{" "}
+        {/* Display the total amount */}
+        <button className="checkout" onClick={checkout}>
+          Checkout
+        </button>
       </div>
     </div>
   );
